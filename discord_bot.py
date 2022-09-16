52c56fed97eadc3cf9171291ce9b8e5131a4ea91
sitzmann <jonasitzmann@hotmail.com>
2022-09-16 22:01:32 +0200

    add echo command to test options

--- discord_bot.py
+++ discord_bot.py
@@ -9,12 +9,24 @@ bot = interactions.Client(
 )
 
 
-
-@bot.command(name='participation', description='loads participation from SpielerPlus')
+@bot.command(name='participation', description='loads participation from SpielerPlus',)
 async def get_participation(ctx: interactions.CommandContext):
     # todo: make sure the messages are not too long
     participation = str(parse.get_participation())
     await ctx.send(str(participation))
+
+
+@bot.command(name='echo', description='echos a given text',
+             options=[
+                 interactions.Option(
+                     name="text",
+                     description="What you want to say",
+                     type=interactions.OptionType.STRING,
+                     required=True,
+                 )]
+             )
+async def echo(ctx: interactions.CommandContext, text='...'):
+    await ctx.send(text)
 
 
 bot.start()
