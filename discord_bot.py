e6da08fb1b9109ba2784bf7213724a45c8924760
sitzmann <jonasitzmann@hotmail.com>
2022-09-23 00:16:05 +0200

    ...

--- discord_bot.py
+++ discord_bot.py
@@ -6,6 +6,7 @@ import splus
 import os
 import utils
 import datetime
+import pickle
 from parse import Participation as P, EventType as E
 intents = discord.Intents.all()
 s = splus.login()
@@ -16,6 +17,7 @@ id_players_info = '1-fHm9B3Gdnnb1uMfUh6m0w_GWnMqBJWNWhk3kBOxaOE'
 players_info = utils.download_google_sheet_as_df(id_players_info).dropna(axis=0)
 splus2discord, discord2splus, members, participation, url2event = {}, {}, [], None, {}
 now = datetime.datetime.now()
+debug = True
 
 def get_user(discord_name):
     username, discrimantor = discord_name.split('#')
@@ -26,13 +28,17 @@ def get_user(discord_name):
 async def on_ready():
     global splus2discord, discord2splus, members
     members = bot.guilds[0].members
-    update_df.start()
+    if debug:
+        load_data_debug()
+    else:
+        update_df.start()
     splus2discord = {r.splus_name: get_user(r.discord_name) for i, r in players_info.iterrows()}
     discord2splus = {v: k for k, v in splus2discord.items()}
+    print('ready')
 
 
 def filter_trainings_func(row):
-    days = (now - url2event[row.url].start).days
+    days = (url2event[row.url].start - now).days
     return days < 14
 
 
@@ -40,6 +46,18 @@ def filter_trainings_func(row):
 async def update_df():
     global url2event, participation
     url2event, participation = parse.get_participation()
+
+
+def load_data_debug():
+    global url2event, participation
+    filename = 'debug_data.pck'
+    if os.path.exists(filename):
+        with open(filename, 'rb') as f:
+            url2event, participation = pickle.load(f)
+    else:
+        with open(filename, 'wb') as f:
+            pickle.dump(parse.get_participation(), f)
+
 
 
 @bot.slash_command(name='tragdichein', description='Listet SpielerPlus Termine auf, zu denen du dich noch nicht eingetragen hast')
@@ -64,17 +82,34 @@ async def get_event_names(ctx):
     non_trainings = [e.name for e in url2event.values() if e.type != E.TRAINING]
     return non_trainings
 
+
+def get_event_participants(event_name, participation_types=None):
+    if participation_types is None:
+        participation_types = [P.YES]
+    participation_types = [p.name.lower() for p in participation_types]
+    url = [e.url for e in url2event.values() if e.name == event_name][0]
+    df = participation[participation.url == url].T
+    df = df[df.iloc[:, 0].isin(participation_types)]
+    names = df.index.tolist()
+    discordnames = [splus2discord[n].mention for n in names if n in splus2discord]
+    return discordnames
+
+janein = {'ja': True, 'nein': False}
+
 @bot.slash_command(name='mention')
 async def autocomplete_example(
     ctx: discord.ApplicationContext,
     event: discord.Option(str, "", autocomplete=get_event_names),
-    # rueckmeldung: discord.Option(str, "", choices='ja'),
+    maybe: discord.Option(str, "", name='vllt', autocomplete=lambda x: ['ja'])='nein',
+    no_response: discord.Option(str, "", name='ka', autocomplete=lambda x: ['ja'])='nein',
+    yes: discord.Option(str, "", name='ja', autocomplete=lambda x: ['nein']) = 'ja',
 ):
-    url = [e.url for e in url2event.values() if e.name == event][0]
-    df = participation[participation.url == url].T
-    df = df[df == 'yes']
-    names = df.index.tolist()
-    discordnames = [splus2discord[n].mention for n in names if n in splus2discord]
-    await ctx.respond(' '.join(discordnames))
+    participation_types = [P.YES]*janein[yes] + [P.MAYBE]*janein[maybe] + [P.Circle]*janein[no_response]
+    discordnames = get_event_participants(event, participation_types=participation_types)
+    if discordnames:
+        await ctx.respond(' '.join(discordnames))
+    else:
+        await ctx.respond('could not find anyone', ephemeral=True)
+
 
 bot.run(token)
