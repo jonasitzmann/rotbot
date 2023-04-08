ca2b70a89b7259e6083678a20aecd0c0baa0e889
Jonas Sitzmann <jonasitzmann@hotmail.de>
2023-04-09 01:09:00 +0200

    decode responses

--- database.py
+++ database.py
@@ -4,7 +4,9 @@ import os
 db = redis.Redis(
   host='redis-19410.c265.us-east-1-2.ec2.cloud.redislabs.com',
   port=19410,
-  password=os.environ.get('REDISPWD'))
+  password=os.environ.get('REDISPWD'),
+  decode_responses=True
+  )
 
 def get_key_names():
-    return list(str(db.get('keys')).split(','))
+    return list(db.get('keys')).split(',')
