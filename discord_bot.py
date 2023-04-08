dab74edd3472513fc472b44f3c034f60fc94e80c
Jonas Sitzmann <jonasitzmann@hotmail.de>
2023-04-09 00:50:12 +0200

    change command names

--- discord_bot.py
+++ discord_bot.py
@@ -165,18 +165,18 @@ async def autocomplete_example(
         await ctx.respond('could not find anyone', ephemeral=True)
 
 
-@bot.slash_command(name='schüsselübergabe')
+@bot.slash_command(name='schuesseluebergabe')
 async def key_to(
     ctx: discord.ApplicationContext,
-    key_name: discord.Option(str, '', autocomplete=get_key_names, name='schlüssel'),
-    receiver: discord.Option(str, '', autocomplete=autocomplete_name, name='emfänger_in')
+    key_name: discord.Option(str, '', autocomplete=get_key_names, name='schluessel'),
+    receiver: discord.Option(str, '', autocomplete=autocomplete_name, name='emfaenger_in')
 ):
     db.set(key_name, receiver)
     sender_name = discord2splus.get(ctx.author, 'Unbekannt')
     await splus2discord[receiver].send(f'{sender_name} hat dir den Schlüssel {key_name} übergeben.')
     await ctx.respond(f'{receiver} hat jetzt den Schüssel "{key_name}"')
 
-@bot.slash_command(name='wosinddieschlüssel')
+@bot.slash_command(name='wosinddieschluessel')
 async def where_are_the_keys(ctx:discord.ApplicationContext):
     await ctx.respond('\n'.join([f"{k}: {db.get(k)}" for k in get_key_names()]))
 
